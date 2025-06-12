31e8127b10c72f38771669c9ea244381cdec91e6
saly <theo.saly@etu.unistra.fr>
2025-06-12 16:15:21 +0200

    bento 96%

--- app/sections/Profile.tsx
+++ app/sections/Profile.tsx
@@ -4,12 +4,12 @@ return (
     <section className="bg-mediumgray rounded-2xl border border-border m-4 backdrop-blur-md relative">
         <img
             className="h-80 md:h-auto object-cover rounded-2xl w-full z-10"
-            src="/profile.png"
+            src="/profile-transparent.png"
             alt=""
         />
         <img
             className="h-80 md:h-auto object-cover rounded-2xl w-full absolute top-0 left-0 opacity-70 z-0"
-            src="/profile2.png"
+            src="/profile.png"
             alt=""
         />
     </section>
